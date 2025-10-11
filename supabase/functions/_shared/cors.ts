a6d51360d59a4e0468a339d8354cc11c3cb834d6
Muhammad Hashfy Habieb <habiebraharjo@Hbbs-MacBook-Air.local>
2025-10-11 22:52:59 +1100

    fix: Add cache-control, expires, pragma to shared CORS headers

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,7 +1,7 @@
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, Authorization, X-Client-Info, ApiKey, Content-Type',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, Authorization, X-Client-Info, ApiKey, Content-Type, cache-control, expires, pragma',
   'Access-Control-Max-Age': '86400',
   'Access-Control-Allow-Credentials': 'true',
 };
